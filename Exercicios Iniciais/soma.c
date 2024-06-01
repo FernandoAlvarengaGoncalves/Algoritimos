ee4b1e8f1c033bd09467ee2823c6a26f3569f7b4
FernandoAlvarengaGoncalves <164833125+FernandoAlvarengaGoncalves@users.noreply.github.com>
2024-06-01 13:35:01 -0300

    soma.c

---
+++ Exercicios Iniciais/soma.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+
+int a, b, soma;
+
+
+scanf("%d", &a);
+scanf("%d", &b);
+
+
+printf("Soma dos valores: \n");
+soma = a+b;
+
+printf("SOMA = R$ %d", soma);
+
+
+
+return 0;
+
+}
