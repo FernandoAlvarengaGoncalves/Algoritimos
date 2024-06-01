db55ff9fd54f1fa0a6191b48f4a81209f8945d72
FernandoAlvarengaGoncalves <164833125+FernandoAlvarengaGoncalves@users.noreply.github.com>
2024-06-01 13:36:34 -0300

    Add files via upload

---
+++ Exercicios Iniciais/teste soma float.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main (){
+	
+	double A, B;
+	int soma;
+	
+	printf("Digite um número");
+	scanf("%lf", &A);
+	
+	prinft("Digite outro número");
+	scanf("%lf", &B);
+	
+	soma = A+B;
+	
+	printf("Resultado = %.4d", soma);	
+	
+	
+	
+	
+	return 0;
+	
+}
