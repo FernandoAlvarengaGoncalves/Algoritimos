aa18e5013b7079306d860071134f85d35d890875
FernandoAlvarengaGoncalves <164833125+FernandoAlvarengaGoncalves@users.noreply.github.com>
2024-06-02 00:45:25 -0300

    Add files via upload

--- Lista2/Ex1For.c
+++ Lista2/Ex1For.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int numero, i;
+
+    printf("Digite um número inteiro: ");
+    scanf("%d", &numero);
+
+	for (i = 2; i <= numero; i=i+2){
+            printf("%d ", i);
+        }
+        
+    
+
+    return 0;
+}
+
 
